5497d9653ccfaedfba54a3d7fd1adb62e706e9b7
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-11-23 20:24:15 +0300

    fix: Исправлена ошибка с фоном во время загрузки

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
+  //const { textLink } = props;
+
   return (
     <header className="header">
       <a href="#" className="header__logo"></a>
+      <Link to="/sign-in" className="link link_type_small">
+        Уже зарегистрированы? Войти
+      </Link>
     </header>
   );
 }
