27778b7b026ebaf6f7a5487e05908826e89a982b
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-11-25 22:11:42 +0300

    fix: Модулю Header изменено расширение на jsx. Отформатирован код. Название функции задано с маленькой буквы

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header(props) {
-
   const { linkName, linkRoute } = props;
 
-  function SignOut() {
+  function signOut() {
     props.signOut();
   }
 
@@ -13,8 +12,8 @@ function Header(props) {
     <header className="header">
       <Link to="/" className="header__logo"></Link>
       <div className="header__container">
-        <p className="header__email">{ props.isLoggedIn && props.userEmail }</p>
-        <Link to={linkRoute} className="link header__link" onClick={SignOut}>
+        <p className="header__email">{props.isLoggedIn && props.userEmail}</p>
+        <Link to={linkRoute} className="link header__link" onClick={signOut}>
           {linkName}
         </Link>
       </div>
