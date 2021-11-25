97e7ad7117b508b0404cb82cd9888cb7e8ac062e
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-11-25 22:13:19 +0300

    fix: Модулю ImagePopup изменено расширение на jsx. Отформатирован код.

---
+++ src/components/ImagePopup.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ImagePopup(props) {
+  return (
+    <div
+      className={`popup popup_type_image
+                  ${Object.keys(props.card).length !== 0 && "popup_opened"}`}
+    >
+      <div className="popup__container">
+        <figure className="popup__container-image">
+          <img
+            className="popup__image"
+            src={props.card.link}
+            alt={props.card.title}
+          />
+          <figcaption className="popup__image-title">
+            {props.card.name}
+          </figcaption>
+        </figure>
+        <button
+          className="popup__icon-close"
+          type="button"
+          onClick={props.onClose}
+        ></button>
+      </div>
+    </div>
+  );
+}
+
+export default ImagePopup;
