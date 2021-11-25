776cd08006dc461cc389079ce021d91e61dd7f7f
Maxim Arzhanov <Arzhanov93@yandex.ru>
2021-11-25 23:13:53 +0300

    fix: Модулю PopupWithForm изменено расширение на jsx. Отформатирован код.

---
+++ src/components/PopupWithForm.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function PopupWithForm(props) {
+  return (
+    <div
+      className={`popup popup_type_${props.name}
+                  ${props.isOpen && "popup_opened"}`}
+    >
+      <div className="popup__container popup__container_type_form">
+        <h2 className="popup__title">{props.title}</h2>
+        <form
+          className="form"
+          name={`form-${props.name}`}
+          onSubmit={props.onSubmit}
+        >
+          {props.children}
+          <button className="form__button" type="submit">
+            {props.isLoading ? `${props.buttonText}...` : `${props.buttonText}`}
+          </button>
+        </form>
+        <button
+          className="popup__icon-close"
+          type="button"
+          onClick={props.onClose}
+        ></button>
+      </div>
+    </div>
+  );
+}
+
+export default PopupWithForm;
